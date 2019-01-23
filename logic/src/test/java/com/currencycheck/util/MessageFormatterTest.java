c8814bd33bf3f1c39a1de3469fb21cd2bfbc0e26
Rhuan Krum <rhuan.krum@hotmail.com>
2019-01-22 20:57:25 -0500

    Add message formatter
    Add message formatter utility class

---
+++ logic/src/test/java/com/currencycheck/util/MessageFormatterTest.java
@@ -0,0 +1,60 @@
+package com.currencycheck.util;
+
+import com.currencycheck.exception.MessageFormatException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MessageFormatterTest {
+
+    @Test
+    public void formatOnNullMessageTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format(null, "Test"), "Error");
+    }
+
+    @Test
+    public void formatOnNoMessageParametersTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format("Test", "Test"), "Error");
+    }
+
+    @Test
+    public void formatOnNullValuesTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format("Test {}", null), "Error");
+    }
+
+    @Test
+    public void formatOnEmptyValuesTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format("Test {}", new String[1]));
+    }
+
+    @Test
+    public void formatOnLessValuesTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format("Test Two Parameters {} {}", "One"));
+    }
+
+    @Test
+    public void formatOnMoreValuesTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format("Test Two Parameters {} {}", "One", "Two", "Three"));
+    }
+
+    @Test
+    public void formatOnSomeValuesAreNullTest(){
+        assertThrows(MessageFormatException.class, () -> MessageFormatter.format("Test Three Parameters {} {} {}", "One", null, "Three"));
+    }
+
+    @Test
+    public void formatOnOneParameter(){
+        assertEquals(MessageFormatter.format("Test One Parameter {}", "One"), "Test One Parameter One");
+    }
+
+    @Test
+    public void formatOnTwoParameters(){
+        assertEquals(MessageFormatter.format("Test Two Parameters {} {}", "One", "Two"), "Test Two Parameters One Two");
+    }
+
+    @Test
+    public void formatOnThreeParameters(){
+        assertEquals(MessageFormatter.format("Test Three Parameters {} {} {}", "One", "Two", "Three"), "Test Three Parameters One Two Three");
+    }
+}
